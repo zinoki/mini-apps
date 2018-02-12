bc672566ff2a8671f9129c0a6561f44d3fc9fd23
Zachary <zinouye@gmail.com>
2018-02-12 15:26:03 -0800

    reset board button working

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,38 @@
+var player_x = 'X';
+var player_o = 'O';
+
+var currentPlayer = player_x;
+
+// Player X starts first
+// app detects win or tie and displays appropriate message 
+// button resets new round of gameplay
+
+// event handler to toggleSquare
+// toggleSquare takes in a player and toggles the square that is clicked on 
+// while the game is not over it concludes the function by changing the event handler to toggle square to other player's piece
+
+
+
+var toggleSquare = function(player) {
+  // input the player's piece in the square that was clicked on
+  var square = document.getElementById("square-1");
+  square.innerHTML = 'Y';
+}
+
+
+
+toggleSquare();
 // event handler
 
 
 
-// create board
-// board will be a matrix of 0 and 1 and 2
-// 0 is empty, 1 is x, and 2 is o
-// event handler will detect clicks and change empty square to have X or O based 
-// on which player clicks
+var resetGame = function() {
+    // grab all elements on table
+    // change innerHTML to be ""
+    var squares = document.getElementsByClassName("square");
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].innerHTML = "";
+    }
+}
 
-console.log('hello');
+// resetGame();
