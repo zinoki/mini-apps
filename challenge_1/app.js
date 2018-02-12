dd7db4048ce6281886bdef2bf61b9c43c59fef2e
Zachary <zinouye@gmail.com>
2018-02-12 12:15:31 -0800

    set up an html board using table

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
+// event handler
 
 
+
+// create board
+// board will be a matrix of 0 and 1 and 2
+// 0 is empty, 1 is x, and 2 is o
+// event handler will detect clicks and change empty square to have X or O based 
+// on which player clicks
+
+console.log('hello');
