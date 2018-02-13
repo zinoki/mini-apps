1d7499a462aa47cc8be1eba9b200feac3ebea130
Zachary <zinouye@gmail.com>
2018-02-13 14:44:49 -0800

    added jquery, handling post requests, page no longer refreshing

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,40 @@
+// ajax request
+
+var server = "http://localhost:3000/";
+// make sure you can grab data from form 
 
 
+var handlePost = function(data) {
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        url: server,
+        success: function(data) {
+            console.log('success');
+            console.log(JSON.stringify(data));
+        }
+    });
+}
+
+$("#jsonForm").submit(function() {
+    console.log($("#jsonData").val())
+    var data = {body: $("#jsonData").val()}
+    handlePost(data);
+    return false;
+})
+
+
+
+
+// $.ajax({
+//     type: "POST",
+//     url: server,
+//     data: data,
+
+// })
+
+// $.post( server, function( data ) {
+//     $( "#test" ).html( data );
+//     console.log(data);
+//   });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 
 app.use(express.static('client'))
+
+
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser());
+
+// app.use(function(req, res) {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.write('you posted', req.body)
+//     res.end(JSON.stringify(req.body, null, 2));
+// })
+
+app.post('/', function(req, res) {
+    var obj = {};
+    console.log('body: ' + JSON.stringify(req.body));
+    res.send(req.body);
+})
+
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
