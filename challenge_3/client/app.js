227e04195fa0fa3418a40615b62bb7a2bbdf566c
Zachary <zinouye@gmail.com>
2018-02-14 15:33:23 -0800

    fixed boardMatrix error

--- challenge_3/client/app.js
+++ challenge_3/client/app.js
@@ -1,13 +1,61 @@
-var App = () => (
-    <div>
-    <h1>hiiii World</h1>
-    <Board />
-    </div>
-);
+// var boardMatrix = new Array(6).fill(new Array(7).fill(0));
+// var boardMatrix = [
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+// ]
 
 
-var Board = () => (
-    <div>hi</div>
-)
+var boardMaker = function(rows, cols) {
+    var boardMatrix = [];
+    for (var i = 0; i < rows; i++) {
+        var row = new Array(cols).fill(0);
+        boardMatrix.push(row);
+    }
+    return boardMatrix;
+}
+
+var boardMatrix = boardMaker(6, 7);
+var nextEmptySquare = function(boardMatrix, colIndex) {
+    for (var rowIndex = boardMatrix.length - 1; i >= 0; i--) {
+        var row = boardMatrix[rowIndex];
+        if (row[colIndex]) {
+            return rowIndex;
+        }
+        // check column index at each row until you find an empty one
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: boardMatrix
+        };
+        this.togglePiece = function(column, player) {
+            // this.state.board[ = player
+        };
+
+    }
+    render() {
+        return (
+        <div>
+          <h1>Connect 404</h1>
+          <Board />
+        </div>
+        );
+    }
+
+}
+
+
+var Board = () => function() {
+  return 
+  (<div>hi</div>);
+}
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/client/compiled/app.js
+++ challenge_3/client/compiled/app.js
@@ -1,18 +1,66 @@
-var App = () => React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'hiiii World'
-    ),
-    React.createElement(Board, null)
-);
+// var boardMatrix = new Array(6).fill(new Array(7).fill(0));
+// var boardMatrix = [
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+// ]
 
-var Board = () => React.createElement(
-    'div',
-    null,
-    'hi'
-);
+
+var boardMaker = function (rows, cols) {
+    var boardMatrix = [];
+    for (var i = 0; i < rows; i++) {
+        var row = new Array(cols).fill(0);
+        boardMatrix.push(row);
+    }
+    return boardMatrix;
+};
+
+var boardMatrix = boardMaker(6, 7);
+var nextEmptySquare = function (boardMatrix, colIndex) {
+    for (var rowIndex = boardMatrix.length - 1; i >= 0; i--) {
+        var row = boardMatrix[rowIndex];
+        if (row[colIndex]) {
+            return rowIndex;
+        }
+        // check column index at each row until you find an empty one
+    }
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: boardMatrix
+        };
+        this.togglePiece = function (column, player) {
+            // this.state.board[ = player
+        };
+    }
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h1',
+                null,
+                'Connect 404'
+            ),
+            React.createElement(Board, null)
+        );
+    }
+
+}
+
+var Board = () => function () {
+    return;
+    React.createElement(
+        'div',
+        null,
+        'hi'
+    );
+};
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
