a0d4640bb4aca95e58e0478232914c00dd221e15
Zachary <zinouye@gmail.com>
2018-02-15 11:50:18 -0800

    class App is rendering correctly

--- challenge_4/client/app.js
+++ challenge_4/client/app.js
@@ -1,5 +1,50 @@
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            frame: 0
+        };
+    }
+    render() {
+        return (
+            <div>
+            <h1>hiiiiiii</h1>
+            </div>
+        );
+    }
+}
+
+var PinSelection = function(props) {
+
+}
+
+
+
+/*
+    1
+   2 3 
+  4 5 6
+ 7 8 9 10
+10 pins
+
+10 frames (turns)
+each frame represents one turn for a bowler
+in a frame a bowler is allowed to roll the ball up to two times
+two chances to knock down all ten pins
+if you fail to knock down all ten pins in one frame, this is an openframe
+and scores the exact number of pins you knock down
+if you knock down all ten pins with two balls, this is a spare
+this scores 10 plus number of pins knocked down with next ball
+if you knock down all ten pins with one ball this is a strike
+this scores 10 plus number of pins knocked down with next two balls (could be up to 30 if two more strikes)
+perfect game is 300 points
+*/
+
+
+// make sure to put components in correct order (ReactDOM.render should always be bottom)
+
 ReactDOM.render(
-    <h1>Hellooo, world!</h1>,
+    <App />,
     document.getElementById('app')
 );
-
