b1fcf1e4d46d4cf37ae6ee1f7e57c514e6adc438
Zachary <zinouye@gmail.com>
2018-02-15 21:37:22 -0800

    set up checkerboard with white and black pieces

--- challenge_5/client/app.js
+++ challenge_5/client/app.js
@@ -1,7 +1,30 @@
-var App = function() {
-    return (
-        <div>yoooo</div>
-    )
+var boardMaker = function() {
+    var board = [
+        [null, 'black', null, 'black', null, 'black', null, 'black'],
+        ['black', null, 'black', null, 'black', null, 'black', null],
+        [null, 'black', null, 'black', null, 'black', null, 'black'],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        ['white', null, 'white', null, 'white', null, 'white', null],
+        [null, 'white', null, 'white', null, 'white', null, 'white'],
+        ['white', null, 'white', null, 'white', null, 'white', null]
+    ]
+    return board;
+}
+var checkerBoard = boardMaker();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: checkerBoard
+        };
+    }
+    render() {
+        console.log(this.state.board);
+        return (
+        <div>hiiiii</div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_5/client/compiled/app.js
+++ challenge_5/client/compiled/app.js
@@ -1,9 +1,23 @@
-var App = function () {
-    return React.createElement(
-        "div",
-        null,
-        "yoooo"
-    );
+var boardMaker = function () {
+    var board = [[null, 'black', null, 'black', null, 'black', null, 'black'], ['black', null, 'black', null, 'black', null, 'black', null], [null, 'black', null, 'black', null, 'black', null, 'black'], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], ['white', null, 'white', null, 'white', null, 'white', null], [null, 'white', null, 'white', null, 'white', null, 'white'], ['white', null, 'white', null, 'white', null, 'white', null]];
+    return board;
 };
+var checkerBoard = boardMaker();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: checkerBoard
+        };
+    }
+    render() {
+        console.log(this.state.board);
+        return React.createElement(
+            'div',
+            null,
+            'hiiiii'
+        );
+    }
+}
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("app"));
